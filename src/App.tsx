724ea75b25144f9a7a954f9756ffbc95cae15f4b
Sanidhya Dobhal <sanidhyadobhal2002@gmail.com>
2024-06-08 11:37:41 +0530

    rectifying the casing in relatedFilesSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import TextEditor from './components/textEditor';
 import ChatSection from './components/aiChatSection/chatSection';
 import Header from './components/header/header';
-import RelatedFilesComp from './components/relatedFilesSection/relatedFilesCom';
+import RelatedFilesComp from './components/relatedFilesSection/relatedFilesComp';
 import { useState } from 'react';
 function App() {
   const [buttonActiveArr, usebuttonActiveArr] = useState([true, false, false, false, true]);
--- src/components/relatedFilesSection/relatedFilesComp.tsx
+++ src/components/relatedFilesSection/relatedFilesComp.tsx
@@ -1,5 +1,5 @@
-import RelatedFilesHeader from './relatedFilesHeade';
-import RelatedFilesBody from './relatedFliesBod';
+import RelatedFilesHeader from './relatedFilesHeader';
+import RelatedFilesBody from './relatedFliesBody';
 export default function Related_files_cont({ buttonActiveArr }: { buttonActiveArr: boolean[] }): JSX.Element {
   return (
     <div id="Related_files_tab" style={{ display: buttonActiveArr[3] ? 'none' : 'block' }}>
