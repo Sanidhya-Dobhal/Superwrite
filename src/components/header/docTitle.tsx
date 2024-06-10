c4e94b756810561153ce23d027b77d6574534cc2
Sanidhya Dobhal <sanidhyadobhal2002@gmail.com>
2024-06-10 18:56:19 +0530

    making the document title editable

--- src/components/header/docTitle.tsx
+++ src/components/header/docTitle.tsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 export default function DocTitle() {
+  const [docTitleState, setdocTitleState] = useState('2024 is the worst year for engineering jobs');
   return (
     <input
       type="text"
       id="title-text"
-      value="2024 is the worst year for engineering jobs"
-      onChange={function () {}}
+      readOnly={false}
+      value={docTitleState}
+      onChange={function (event) {
+        setdocTitleState(event.target.value);
+      }}
+      onKeyDown={function (event) {
+        if (event.key === 'Enter') {
+          (event.target as HTMLInputElement).blur();
+        }
+      }}
     ></input>
   );
 }
